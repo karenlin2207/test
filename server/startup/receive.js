a905a72d1ee48e2e7daca9f1336ad69ef2d7b7d4
karenlin2207 <ddrenlin@gmail.com>
2016-09-12 17:31:23 +0800

    update#3

--- server/startup/receive.js
+++ server/startup/receive.js
@@ -1,14 +1,16 @@
-WebApp.connectHandlers.use("/receive", function(req, res, next) {
-  var body = [];
-  var MerchantID = res.body;
-  req.on('data', Meteor.bindEnvironment(function (data) {
-    body.push(data.toString());
-  }));
+export default function () {
+  WebApp.connectHandlers.use("/receive", function(req, res, next) {
+    var body = [];
+    var MerchantID = res.body;
+    req.on('data', Meteor.bindEnvironment(function (data) {
+      body.push(data.toString());
+    }));
 
-  req.on('end', Meteor.bindEnvironment(function () {
-    console.log(req.method);
-    if (MerchantID) console.log(MerchantID);
-    res.writeHead(200);
-    res.end("Hello world from: " + body + '\n');
-  }));
-});
+    req.on('end', Meteor.bindEnvironment(function () {
+      console.log(req.method);
+      if (MerchantID) console.log(MerchantID);
+      res.writeHead(200);
+      res.end("Hello world from: " + body + '\n');
+    }));
+  });
+}
