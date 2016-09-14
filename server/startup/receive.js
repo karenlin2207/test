7ac9351d2375df08e77d20c15806b2d1e036e78f
karenlin2207 <ddrenlin@gmail.com>
2016-09-14 15:02:57 +0800

    update

--- server/startup/receive.js
+++ server/startup/receive.js
@@ -13,7 +13,6 @@ WebApp.connectHandlers.use("/receive", function(req, res, next) {
   req.on('end', Meteor.bindEnvironment(function () {
     console.log(body);
     res.writeHead(200, {'Content-Type': 'application/json'});
-    if (MerchantID) console.log(MerchantID);
     res.end("Hello world from: " + body + '\n');
   }));
 });
