72b4ab97699af70a374658db293f731c9fe817f0
karenlin2207 <ddrenlin@gmail.com>
2016-09-21 17:25:59 +0800

    change

--- server/startup/receive.js
+++ server/startup/receive.js
@@ -37,7 +37,7 @@ WebApp.connectHandlers.use("/receive", function(req, res, next) {
       temparray.push(obj);
     }
     console.log(obj);
-    console.log(Orders.findOne({cartId:obj.MerchantTradeNo}));
+    console.log(Orders.findOne());
 
     res.writeHead(200, {'Content-Type': 'application/json'});
     res.end("Hello world from: " + body + '\n');
