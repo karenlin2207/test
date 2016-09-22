dc9adc3df190f4cccd7842421bbc277e1d9f551f
karenlin2207 <ddrenlin@gmail.com>
2016-09-22 12:17:42 +0800

    change

--- server/startup/receive.js
+++ server/startup/receive.js
@@ -48,24 +48,7 @@ WebApp.connectHandlers.use("/receive", function(req, res, next) {
 
     
 
-      var Allpay = require("allpay");
-      var allpay = new Allpay({
-        merchantID: data.settings.merchantID || "2000132",
-        hashKey: data.settings.hashKey || "5294y06JbISpM5x9",
-        hashIV: data.settings.hashIV || "v77hoKGq4kWxNNIS",
-        mode: "test",
-        debug: true
-      });
-
-      allpay.setHost({
-        baseUrl: "payment-stage.allpay.com.tw",
-        port: 80,
-        useSSL: false
-      });
-      result = allpay.isDataValid(obj);
-      result = allpay.isDataValid(obj, "SHA256");
-
-      orders = Orders.findOne({cartId:obj.MerchantTradeNo});
+    orders = Orders.findOne({cartId:obj.MerchantTradeNo});
     Orders.update({
       "cartId":obj.MerchantTradeNo,
       "billing.paymentMethod.transactionId": orders.billing[0].paymentMethod.transactionId
