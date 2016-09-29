1fa1764c5d7dc4f4c6d433242b1ea92b486ea158
karenlin2207 <ddrenlin@gmail.com>
2016-09-29 19:04:51 +0800

    test

--- imports/plugins/included/rolessetting/server/methods.js
+++ imports/plugins/included/rolessetting/server/methods.js
@@ -1,5 +1,6 @@
 import { Roles } from "/lib/collections";
 import * as Collections from "/lib/collections";
+import { Roles } from "/lib/collections";
 import { Reaction } from "/server/api";
 
 Meteor.methods({
