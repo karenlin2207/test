eca87928020e67ecdb5908be450b76ce5adddfdb
karenlin2207 <ddrenlin@gmail.com>
2016-09-29 19:23:38 +0800

    test

--- imports/plugins/included/rolessetting/server/methods.js
+++ imports/plugins/included/rolessetting/server/methods.js
@@ -1,29 +1,28 @@
 import { Roles } from "/lib/collections";
 import * as Collections from "/lib/collections";
-
 import { Reaction } from "/server/api";
 
 Meteor.methods({
  addRoles: function (doc) {
 
     console.log("[addRoles][doc]", doc);
-    check(doc,String);
-    var obj = {rolesName:doc};
-    /*
+
     check(doc, {
 	rolesName: String,
 	//permissions: [String],
 	//shopId: String
     });
-    */
 
 
+/*
     if (!Reaction.hasPermission("shipping")) {
       throw new Meteor.Error(403, "Access Denied");
     }
     console.log(Collections.Accounts.find().fetch());
-    Roles.insert(obj);
-
+    Roles.insert({
+        rolesName: "test"
+    });
+*/
     //return true;
   }
 });
