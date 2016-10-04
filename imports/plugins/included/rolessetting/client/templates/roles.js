140baa90134af09952e97e48d491a8cc51dd23fd
karenlin2207 <ddrenlin@gmail.com>
2016-10-04 15:36:21 +0800

    update

--- imports/plugins/included/rolessetting/client/templates/roles.js
+++ imports/plugins/included/rolessetting/client/templates/roles.js
@@ -146,7 +146,7 @@ Template.editRoles.helpers({
   },
   permissionGroups: function (thisShopId) {
     let permissionGroups = [];
-    const shopId = Meteor.getShopId() || Template.currentData();
+    const shopId = Reaction.getShopId() || Template.currentData();
     const packages = Packages.find({
       shopId: shopId
     });
